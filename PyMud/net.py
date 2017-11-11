1cad8ef611378b3be5f835026e6baaf37340d1d3
Jason Hill <qbfreak@qbfreak.net>
2017-11-10 21:19:56 -0500

    Rearrange for cleanliness, added some doc

--- PyMud/net.py
+++ PyMud/net.py
@@ -11,22 +11,29 @@ class Network(multiqueue.MultiQueue, threading.Thread):
         self.port = port
 
     def console(self, msg):
+        "Enqueue a message for console output"
         self.enqueue('console', str(msg))
 
+    def shutdown(self):
+        "Enqueue the shutdown command in the command queue"
+        self.enqueue('control', "shutdown", newline=False)
+
     def run(self):
+        """
+        Network main thread
+        """
         self.console("Networking started on port " + str(self.port))
-        runto = time.mktime(time.localtime()) + 5
-        while time.mktime(time.localtime()) <= runto:
+        while True:
+            # Don't max out the processor with our main loop
             time.sleep(0.1)
+            # Check the control queue for commands
             while self.hasqueued('control'):
                 cmd = self.get_nowait('control')
                 if cmd == 'shutdown':
+                    # Shut down the network thread
                     self.console('Shutting down')
                     self.console("Networking stopped")
                     return
                 else:
+                    # Whaaat? I don't know how to do that
                     self.console("WARNING: Unknown command issued to Network: " + str(cmd))
-        self.console("Networking stopped")
-
-    def shutdown(self):
-        self.enqueue('control', "shutdown", newline=False)
